48ece78c4b31bdf45765aa05cc0f1adf3e316571
lokin7778 <lokin7778@gmail.com>
2025-10-28 08:15:05 -0400

    Created the required arraylist variables and then created a while loop to parse through the program until the user asks to stop, and then created the prompts for the user and then inputted their choice

--- SCOREBOARD_ArrayLists/src/Scoreboard.java
+++ SCOREBOARD_ArrayLists/src/Scoreboard.java
@@ -5,6 +5,33 @@ public class Scoreboard{
 
     public static void main(String[] args){
 
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in); // creates a scanner object 'in'
+
+        // create all the necessary arraylist variables
+        ArrayList<String> playerNames = new ArrayList<>();
+        ArrayList<Integer> playerScores = new ArrayList<>();
+
+        boolean flag = true; // creates a flag variable to be the loop variable for the while loop next
+
+        while(flag){
+
+            // prompt the user with the given options
+            System.out.println("Select an option: ");
+            System.out.println("1. Add Player");
+            System.out.println("2. Delete Player");
+            System.out.println("3. View Players");
+            System.out.println("4. Exit");
+
+            // prompt the user to enter their choice
+
+            System.out.print("Enter your choice [1/2/3/4]: ");
+            int choice = in.nextInt();
+            in.nextLine();
+
+
+
+        }
+
+
     }
 }
