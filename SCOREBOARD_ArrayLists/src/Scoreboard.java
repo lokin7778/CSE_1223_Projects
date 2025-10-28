e91870cca424445233b1fab0921951dde171e546
lokin7778 <lokin7778@gmail.com>
2025-10-28 08:06:02 -0400

    Added the skeleton code for the project by importing the necessary modules and creating a Scanner Object 'in'

---
+++ SCOREBOARD_ArrayLists/src/Scoreboard.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Scoreboard{
+
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+    }
+}
